e5e7b3863aef0c93e40359bfefa57a101b2dd12d
bhupendra <bhupendra.bisht59.com>
2022-09-02 16:17:43 +0530

    adding arduino side code

--- Arduino Code/HC_05/HC_05.ino
+++ Arduino Code/HC_05/HC_05.ino
@@ -1,10 +1,64 @@
+/*  
+  Bit pattern for transmission #
+  from left to right
+  First Bit: Information type
+  Second Bit: Information type
+  Third Bit: Information type
+  000 for button
+  010 for IMU
+  111 or 224 for Joystick X
+  110 or 192 for Joystick Y
+  Others are reserved
+
+  If Joystick: 
+  Fourth - Eight Bit: Value of Joystick
+*/
+
+int VRx = A0;
+int VRy = A1;
+int SW = 2;
+
+int xPosition = 0;
+int yPosition = 0;
+int SW_state = 0;
+byte mapX = 0;
+byte mapY = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+ Serial.begin(9600);
+   pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(SW, INPUT_PULLUP); 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  xPosition = analogRead(VRx);
+  yPosition = analogRead(VRy);
+  SW_state = digitalRead(SW);
+  /*
+    mapX = map(xPosition, 0, 1023, -512, 31);
+    mapY = map(yPosition, 0, 1023, -512, 512);
+  */
+  mapX = map(xPosition, 0, 1023, 0, 31);
+  mapY = map(yPosition, 0, 1023, 0, 31);
 
+  sendJoyStickX(mapX);
+  sendJoyStickY(mapY);
+
+  //Serial.println(mapX);
+  //Serial.println(mapY);
+  
+  delay(100);
+//  Serial.println("data from HC 05");
+}
+
+void sendJoyStickX(int mapX){
+  byte flagBit = 224;
+  Serial.println(flagBit|mapX); // ensure that mapX dont have top 3 MSB set
+}
+
+void sendJoyStickY(int mapY){
+  byte flagBit = 192;
+  Serial.println(flagBit|mapY); // ensure that mapX dont have top 3 MSB set
 }
